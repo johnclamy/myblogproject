85288e8e472ace5b008e2f8851a4519769b21d6b
johnclamy <johnclamy@gmail.com>
2025-03-18 21:54:49 +0000

    define an enumeration class Status with values are DF and PB

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,12 +3,17 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     publish = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
