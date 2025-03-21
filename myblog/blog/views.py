933c2451fa23ec0626ae6c3394a055b880db7d81
johnclamy <johnclamy@gmail.com>
2025-03-21 23:22:14 +0000

    setup the post detailed view template for the './'blog/views.py'

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 
 
@@ -6,3 +7,12 @@ def post_list(request):
     posts = Post.objects.all()
 
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No post found.')
+    
+    return render(request, 'blog/post/detail.html', {'post': post})
