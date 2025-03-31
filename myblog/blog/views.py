f27bd57779f3ace5243b8753b7a584255ca772aa
johnclamy <johnclamy@gmail.com>
2025-03-31 12:44:57 +0100

    convert the view layer to accept a class based view

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,33 +1,42 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import get_object_or_404, render
+# from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+# from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.views.generic import ListView
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page')
-
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # if the page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        publish__year=year,
-        publish__month=month,
-        publish__day=day
-    )
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page')
+
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # if the page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # if page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+
+#     return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+# def post_detail(request, year, month, day, post):
+#     post = get_object_or_404(
+#         Post,
+#         status=Post.Status.PUBLISHED,
+#         slug=post,
+#         publish__year=year,
+#         publish__month=month,
+#         publish__day=day
+#     )
     
-    return render(request, 'blog/post/detail.html', {'post': post})
+#     return render(request, 'blog/post/detail.html', {'post': post})
