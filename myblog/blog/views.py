c9d46acbfff4623834fa3fe74818e78ca75be518
johnclamy <johnclamy@gmail.com>
2025-04-01 11:01:38 +0100

    create an instance of the email post form and handle the form submission

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,8 @@
-# from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-# from django.shortcuts import get_object_or_404, render
-from .models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -12,31 +13,45 @@ class PostListView(ListView):
 
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page')
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page')
 
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         # if the page_number is not an integer get the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # if page_number is out of range get last page of results
-#         posts = paginator.page(paginator.num_pages)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if the page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
 
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-# def post_detail(request, year, month, day, post):
-#     post = get_object_or_404(
-#         Post,
-#         status=Post.Status.PUBLISHED,
-#         slug=post,
-#         publish__year=year,
-#         publish__month=month,
-#         publish__day=day
-#     )
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
     
-#     return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
