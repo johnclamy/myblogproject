74e4379e47e4c475731d1e245027913aee2eca21
johnclamy <johnclamy@gmail.com>
2025-04-02 12:25:30 +0100

    enable the post_share view function to send emails

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from .models import Post
@@ -45,13 +46,19 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
 
     if request.method == 'POST':
         # form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} ({cd['email']}) recommends you read {post.title}."
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments: {cd['message']}"
+            send_mail(subject=subject, message=message, from_email=None, recipient_list=[cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
